4fe2cc410e67cdf06194630a08b48ddc2193359f
FreeDutta <jagwarisdutta@gmail.com>
2025-12-28 16:58:58 +0530

    table of 9 with %n

---
+++ src/ClassTasks/task_17_Dec_Diff_way.java
@@ -0,0 +1,17 @@
+package ClassTasks;
+
+public class task_17_Dec_Diff_way {
+    static void main(String[] args) {
+        byte t = 9;
+        System.out.printf("%d*1=%d%n",t,(t*1));
+        System.out.printf("%d*2=%d%n",t,(t*2));
+        System.out.printf("%d*3=%d%n",t,(t*3));
+        System.out.printf("%d*4=%d%n",t,(t*4));
+        System.out.printf("%d*5=%d%n",t,(t*5));
+        System.out.printf("%d*6=%d%n",t,(t*6));
+        System.out.printf("%d*7=%d%n",t,(t*7));
+        System.out.printf("%d*8=%d%n",t,(t*8));
+        System.out.printf("%d*9=%d%n",t,(t*9));
+        System.out.printf("%d*10=%d%n",t,(t*10));
+    }
+}
